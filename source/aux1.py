238980afd73eeb72c01b448c39562ba28153c8d1
Sersan Guedes <sersan.guedes@RIOENERGY.LOCAL>
2020-09-27 18:06:38 -0300

    parte1 done

--- source/aux1.py
+++ source/aux1.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import TimeSeriesSplit
+
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
 
@@ -22,6 +25,7 @@ def FNC_insert_N_Features( df, N ):
 
     return df
 
+
 def FNC_Xy_portland( N ):
     path_file = r"C:\Users\sersan.guedes\Desktop\exerc_Semana03\data\portland-oregon.csv"
     df1 = pd.read_csv(path_file, index_col='Month', parse_dates=['Month'])
@@ -38,62 +42,38 @@ def FNC_Xy_portland( N ):
 
 
 def FNC_runTotal( N ):
-    N = 4
+
     X, y, df1, df2 = FNC_Xy_portland( N )
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
     reg = LinearRegression()
 
-    reg.fit(X, y)
-    y_pred = reg.predict(X)
+    reg.fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
 
-    rmse1 = mean_squared_error( y_pred, y, squared=False )
+    rmse1 = mean_squared_error( y_pred, y_test, squared=False )
 
     x_proxMes = df1.iloc[-N:,0].values
     x_proxMes = np.array( x_proxMes ).reshape( 1, -1 )
 
     y_proxMes = reg.predict( x_proxMes )
-    a = reg.predict( np.array([1425, 1419, 1432, 1394]).reshape(1, -1) )
+    #a = reg.predict( np.array([1425, 1419, 1432, 1394]).reshape(1, -1) )
 
     print("RMSE: ", rmse1.round(2))
 
-    print("Previsão de nº de pessoas em 1969-06 (gab. é 1327): ", a)
+    #print("Previsão de nº de pessoas em 1969-06 (gab. é 1327): ", a)
     print("Previsão de nº de pessoas em 1969-07: ", y_proxMes)
 
     plt.figure(101)
-    plt.plot( y.index, y, label='y' )
-    plt.plot( y.index, y_pred, label='y_pred' )
+    plt.plot( y_test.index, y_test, '*', label='y_test' )
+    plt.plot( y_test.index, y_pred, '*', label='y_pred' )
     plt.legend()
     plt.show()
 
     print("FIM!")
 
-    return y, y_pred
+    return y_test, y_pred
 
 
-
-
-#N = 4
-#X, y, df1, df2 = FNC_Xy_portland( N )
-
-#reg = LinearRegression()
-
-#reg.fit(X, y)
-#y_pred = reg.predict(X)
-
-#mean_squared_error( y_pred, y )
-
-#x_proxMes = df1.iloc[-N:,0].values
-#x_proxMes = np.array( x_proxMes ).reshape( 1, -1 )
-
-#y_proxMes = reg.predict( x_proxMes )
-#a = reg.predict( np.array([1425, 1419, 1432, 1394]).reshape(1, -1) )
-
-#print("FOI!")
-
-#plt.figure(101)
-#plt.plot( y, label='y' )
-#plt.plot( y.index, y_pred, label='y_pred' )
-#plt.legend()
-#plt.show()
-
-#print("FOI2!")
+#def FNC_mnist():
